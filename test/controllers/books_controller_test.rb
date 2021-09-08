d14d30d6545dc311ce8b913a447586ad19005166
misono <ginpeimisonou@gmail.com>
2021-09-08 09:27:38 +0900

    books_controllerテストの修正

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -3,8 +3,13 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
+  # sign_in ヘルパーを利用するため
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @book = books(:one)
+    @user = users(:test_user)
+    sign_in @user
   end
 
   test 'should get index' do
