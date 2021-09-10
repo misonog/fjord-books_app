1f3c7d33189f6398fc30a361ab8ea0d99a92dcbf
misono <ginpeimisonou@gmail.com>
2021-09-10 22:05:30 +0900

    ユーザー更新のシステムテスト追加

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -26,4 +26,20 @@ class UsersTest < ApplicationSystemTestCase
     visit users_path
     assert_selector 'h1', text: 'ユーザー'
   end
+
+  test 'updating a user' do
+    visit new_user_session_path
+    fill_in 'user_email', with: @user.email
+    fill_in 'user_password', with: 'password'
+    click_button 'ログイン'
+    visit users_path
+    click_link '編集'
+
+    fill_in '現在のパスワード', with: 'password'
+    fill_in '郵便番号', with: 123_456_7
+    click_on '更新'
+
+    assert_text 'アカウント情報を変更しました。'
+    assert page.assert_current_path(user_path(@user))
+  end
 end
