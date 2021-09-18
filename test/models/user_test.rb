d09e800e5e46dc65d5ae5c53b531dd5bc71198e6
misono <ginpeimisonou@gmail.com>
2021-09-18 14:00:19 +0900

    Userモデルのテスト追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '名前が設定されている場合は名前を返す' do
+    user = users(:alice)
+    assert_equal 'alice', user.name_or_email
+  end
+
+  test '名前が設定されていない場合はメールアドレスを返す' do
+    user = users(:alice)
+    user.name = ''
+    assert_equal 'alice@example.com', user.name_or_email
+  end
+
+  test 'ユーザーをfollowとunfollowできる' do
+    alice = users(:alice)
+    bob = users(:bob)
+    assert_not alice.following?(bob)
+    assert_not bob.followed_by?(alice)
+
+    alice.follow(bob)
+    assert alice.following?(bob)
+    assert bob.followed_by?(alice)
+
+    alice.unfollow(bob)
+    assert_not alice.following?(bob)
+    assert_not bob.followed_by?(alice)
+  end
 end
